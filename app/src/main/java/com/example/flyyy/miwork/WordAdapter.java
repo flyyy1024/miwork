088b3763f527ce703d42fe19f56aaec18b6b5f98
flyyy <flyyy1024@gmail.com>
2018-12-27 12:42:57 +0800

    删去多余的包

--- app/src/main/java/com/example/flyyy/miwork/WordAdapter.java
+++ app/src/main/java/com/example/flyyy/miwork/WordAdapter.java
@@ -1,8 +1,6 @@
 package com.example.flyyy.miwork;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

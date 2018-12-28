e7fe1aad4ad2656c7a52a4a27c32c30b6a358a1f
inpure <chfjinqh@gmail.com>
2018-12-28 20:56:14 +0800

    添加raw资源

--- app/src/main/java/com/example/flyyy/miwork/NumbersActivity.java
+++ app/src/main/java/com/example/flyyy/miwork/NumbersActivity.java
@@ -1,13 +1,17 @@
 package com.example.flyyy.miwork;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    private MediaPlayer mMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,14 @@ public class NumbersActivity extends AppCompatActivity {
         WordAdapter itermsAdapter = new WordAdapter(this, words, R.color.category_numbers);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(itermsAdapter);
+
+        mMediaPlayer = MediaPlayer.create(this, R.raw.number_one);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mMediaPlayer.start();
+            }
+        });
 
     }
 }
